db3d70ba2840677e2e8e51642bf1fcf63e1ebf61
Aanyajaiswal <aanyajaiswal21@gmail.com>
2026-02-20 10:33:19 +0530

    class work

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
-console.log("Hello World");
-document.write("Hello World");
+// console.log("Hello World");
+// document.write("Hello World");
+
+// PRIMITIVE TYPE
+// var num=10;
+// console.log(num);
+// console.log(typeof(num));
+// num=false;
+// console.log(num);
+// console.log(typeof(num));
+// num="AANYA";
+// console.log(num);
+// console.log(typeof(num));
+// num=null;
+// console.log(num);
+// console.log(typeof(num));
+// num=undefined;
+// console.log(num);
+// console.log(typeof(num));
+
+
+<script type="text/javascript">
+const sym1=Symbol(4);
+console.log(sym1);
+const sym2=Symbol(4);
+console.log(sym2);
+if(sym1==sym2){
+    console.log("true")
+}
+else{
+    console.log("false")
+}
+</script> 
