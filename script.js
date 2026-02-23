5d3fa8eff709ee0808576879685f2bf358bb4012
Aanyajaiswal <aanyajaiswal21@gmail.com>
2026-02-23 15:42:13 +0530

    loops

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // console.log("Hello World");
 // document.write("Hello World");
 
-// PRIMITIVE TYPE
+// // PRIMITIVE TYPE
 // var num=10;
 // console.log(num);
 // console.log(typeof(num));
@@ -19,15 +19,53 @@
 // console.log(typeof(num));
 
 
-<script type="text/javascript">
-const sym1=Symbol(4);
-console.log(sym1);
-const sym2=Symbol(4);
-console.log(sym2);
-if(sym1==sym2){
-    console.log("true")
+// {/* <script type="text/javascript">
+// const sym1=Symbol(4);
+// console.log(sym1);
+// const sym2=Symbol(4);
+// console.log(sym2);
+// if(sym1==sym2){
+//     console.log("true")
+// }
+// else{
+//     console.log("false")
+// }
+// </script>  
+//  */}
+
+
+//   <script type="text/javascript">
+// var a= 10;
+// var b=20;
+// var linebreak="<br/>;
+
+// document.write(result);
+// document.write(linbreak);
+
+ 
+// </script>
+
+// {/* <script type ="text/javascript">
+//    var count;
+//    document.write("Starting Loop"+"<br/>"),
+//    for(count=0;count<10;count++){
+//     document.write("Current count:"+count),
+//     document.write(linebreak)
+//    }
+//    document.write("Loop stopped"),
+
+// </script> */}
+
+
+
+// for(key in object){
+
+// }
+const person={
+    fname: "Ajay",
+    lname:" Singh",
+    age:25
+};
+for(let x in person){
+    console.log("person details:"+ x +":"+ person[x])
 }
-else{
-    console.log("false")
-}
-</script> 
